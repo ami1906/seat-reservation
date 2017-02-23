cac2ea01c7300e6e6168e896cefc729d823cb37b
Amarnath <amarnath.k@caratlane.com>
2017-02-23 19:09:38 +0530

    removed unwanted node modules

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 m = require('mithril');
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,14 +18,9 @@ app.listen(PORT, function () {
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
